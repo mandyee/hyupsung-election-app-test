5eb643cbc1a642ef4b821ca0069b8c114d5d6e76
mandyee <minju4144@gmail.com>
2020-06-19 09:37:45 +0900

    Reset

--- webpack.config.js
+++ webpack.config.js
@@ -7,8 +7,7 @@ module.exports = {
   },
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: 'build.js',
-    publicPath: '/'
+    filename: 'build.js'
   },
   module: {
     rules: [
